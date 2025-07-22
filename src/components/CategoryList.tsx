f81dd1542d29e8447c8bd40ab1d9d6666391f006
tiethuong <tietlevi01012004@gmail.com>
2025-07-22 18:12:08 +0700

    categorylist

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,20 +1,28 @@
 import { Table } from "antd";
-import { Header } from "antd/es/layout/layout";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Spin } from "antd";
 
 function CategoryList() {
-  const data = [
-    { id: 1, name: "Laptop" },
-    { id: 2, name: "Điện thoại" },
-  ];
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["categories"],
+    queryFn: async () => {
+      const res = await axios.get("http://localhost:3000/categories");
+      return res.data;
+    },
+  });
+
 
   const columns = [
     { title: "ID", dataIndex: "id" },
     { title: "Tên danh mục", dataIndex: "name" },
   ];
+  if (isError) return <p>Lỗi tải danh mục!</p>;
+  if (isLoading) return <Spin />;
+
 
   return (
     <div>
-      <Header />
       <h2>Danh sách danh mục</h2>
       <Table dataSource={data} columns={columns} rowKey="id" />
     </div>
